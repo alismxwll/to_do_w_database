878641fdb0b8272aef20834d8b3833dc98852b78
Andrew Akers & Blake Mason <student@epicodus.com>
2014-08-04 12:25:06 -0700

    saves a list to a database and retrieves all the lists in the database

--- lib/list.rb
+++ lib/list.rb
@@ -12,7 +12,18 @@ class List
     @name = another_list.name
   end
 
-  def List.all
-    []
+  def self.all
+    results = DB.exec("SELECT * FROM lists;")
+    lists = []
+    results.each do |result|
+      name = result['name']
+      lists << List.new(name)
+    end
+    lists
   end
+
+  def save
+    DB.exec("INSERT INTO lists (name) VALUES ('#{@name}');")
+  end
+
 end
